0f1ef83b5b468478ff35d0f9a217c99ca8f4ca04
hasanmint73 <hasanmint73@gmail.com>
2022-05-23 17:22:08 +0600

    initial product loaded

--- src/Pages/Products/Products.js
+++ src/Pages/Products/Products.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Product from './Product';
 
 const Products = () => {
-    
+    const [products, setProducts] = useState([]);
+
+
+    useEffect(() => {
+        fetch('data.json')
+            .then(Response => Response.json())
+            .then(data => setProducts(data))
+    }, [])
+
     return (
-        <div className='text-center my-10'>
-            <h3 className='text-secondary text-xl font-bold '>Our Products </h3>
-        </div>
+        <>
+            <div className="mx-auto px-12 my-28">
+                <div className='text-center my-10'>
+                    <h3 className='text-secondary text-5xl font-bold '>Our Products:{products.length} </h3>
+                </div>
+                <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10'>
+                    {
+                        products.map(product =>
+                            <Product
+                                key={product._id}
+                                product={product}
+                            ></Product>)
+                    }
+                </div>
+            </div>
+
+        </>
     );
 };
 

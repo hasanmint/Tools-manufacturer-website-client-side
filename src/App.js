93acb2f06f58e76b7014d18e131761c9330561c3
hasanmint73 <hasanmint73@gmail.com>
2022-05-23 11:58:45 +0600

    initial route ready commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Pages/Home/Home';
+import Header from './Shared/Header';
+import Footer from './Shared/Footer';
 
 function App() {
   return (
     <div className="App">
-      <h2>Start project</h2>
+      <>
+        <Header></Header>
+        <Routes>
+          <Route index element={<Home />} />
+        </Routes>
+        <Footer></Footer>
+      </>
     </div>
   );
 }
